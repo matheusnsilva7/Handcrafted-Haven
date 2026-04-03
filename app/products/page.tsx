e360728749e946823bcad930dce845b12cfdee5a
Matheus <silva.matheus6081@gmail.com>
2026-04-02 22:25:27 -0300

    database

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,8 +1,8 @@
 import Link from 'next/link';
-import { getProducts, deleteProduct } from '@/app/lib/actions';
+//import { getProducts, deleteProduct } from '@/app/lib/actions';
 
 export default async function ProductsPage() {
-  const products = await getProducts();
+  //const products = await getProducts();
 
   return (
     <div>
@@ -10,17 +10,17 @@ export default async function ProductsPage() {
 
       <Link href="/products/create">Create Product</Link>
 
-      {products.map((p: any) => (
+      {/* {products.map((p: any) => (
         <div key={p.id}>
           <h3>{p.name}</h3>
 
           <Link href={`/products/${p.id}/edit`}>Edit</Link>
-          <form action={deleteProduct}>
+          <form action={deleteProduct} >
             <input type="hidden" name="id" value={p.id} />
             <button type="submit">Delete</button>
           </form>
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }
